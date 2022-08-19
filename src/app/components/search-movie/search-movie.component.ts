842f2c53270d6ef57d32496c041cead09e14ae55
Michel Vidailhet <michel.vidailhet@gmail.com>
2022-08-19 21:01:30 +0200

    Création d'un formulaire basique avec l'identifiant et le titre en utilisant le FormBuilder
    Affichage dans la console d'objet searchFormGroup

--- src/app/components/search-movie/search-movie.component.ts
+++ src/app/components/search-movie/search-movie.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-search-movie',
   templateUrl: './search-movie.component.html',
-  styleUrls: ['./search-movie.component.scss']
+  styleUrls: ['./search-movie.component.scss'],
 })
 export class SearchMovieComponent implements OnInit {
+  searchForm!: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) {
+    this.initSearchFormGroup();
   }
 
+  initSearchFormGroup() {
+    this.searchForm = this.formBuilder.group({
+      id: [],
+      title: [],
+    });
+  }
+
+  ngOnInit(): void {}
+
+  onSubmit() {
+    console.log(this.searchForm);
+  }
 }
