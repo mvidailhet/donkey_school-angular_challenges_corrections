7a9aa8e976044750f947431857046717e1e0b6ed
Michel Vidailhet <michel.vidailhet@gmail.com>
2022-08-20 10:04:37 +0200

    Création du component onomatopeia

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Biernvenue sur le site de Michel !';
+  onomatopeias: string[] = [];
 }
---
+++ src/app/create-onomatopeia/create-onomatopeia.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-onomatopeia',
+  templateUrl: './create-onomatopeia.component.html',
+  styleUrls: ['./create-onomatopeia.component.scss']
+})
+export class CreateOnomatopeiaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
