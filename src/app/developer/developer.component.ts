c7b762b5b96fa3c3e043c0305ee0cfdc1621d907
Michel Vidailhet <michel.vidailhet@gmail.com>
2022-08-20 09:45:04 +0200

    Affichage des données du développeur dans la vue

--- src/app/developer/developer.component.ts
+++ src/app/developer/developer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Developer } from '../common/developer.model';
 
 @Component({
   selector: 'app-developer',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./developer.component.scss']
 })
 export class DeveloperComponent implements OnInit {
+  developer?: Developer;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.developer = {
+      firstName: 'Michel',
+      lastName: 'Vidailhet',
+      age: 36,
+      bio: 'Un dev de ouf',
+      sexe: 'homme',
+      skills: [],
+    }
   }
 
 }
