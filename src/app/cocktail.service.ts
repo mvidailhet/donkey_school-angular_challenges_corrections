4615ca8a0198151a62ced8488da1af1999ef619f
Michel Vidailhet <michel.vidailhet@gmail.com>
2022-08-20 10:55:33 +0200

    Ajout de cocktails dans le tableau de cocktails du service

--- src/app/cocktail.service.ts
+++ src/app/cocktail.service.ts
@@ -5,7 +5,18 @@ import { Cocktail } from './cocktail.model';
   providedIn: 'root'
 })
 export class CocktailService {
-  cocktails: Cocktail[] = [];
+  cocktails: Cocktail[] = [
+    {
+      name: 'mojito',
+      price: 12,
+      img: 'https://media.istockphoto.com/photos/mojito-cocktail-picture-id157311412?k=20&m=157311412&s=612x612&w=0&h=yEf4Ny1E7taHCJfmhF4TAYjbA4jXsPsXc9HGrGGNwiU='
+    },
+    {
+      name: 'sex on the beach',
+      price: 14,
+      img: 'https://images.assetsdelivery.com/compings_v2/chandlervid85/chandlervid851805/chandlervid85180500060.jpg'
+    }
+  ];
 
   getCocktails() {
     return this.cocktails;
