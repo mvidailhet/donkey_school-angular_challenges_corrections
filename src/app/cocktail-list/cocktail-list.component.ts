004eefe79999206aedae325b2bcc0981cf4d6eb1
Michel Vidailhet <michel.vidailhet@gmail.com>
2022-08-20 10:49:18 +0200

    Création du composant cocktail-list ng g c cocktail-list --skip-tests

---
+++ src/app/cocktail-list/cocktail-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cocktail-list',
+  templateUrl: './cocktail-list.component.html',
+  styleUrls: ['./cocktail-list.component.scss']
+})
+export class CocktailListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
