2a9b7239d18c23a3b7c81f06e5e600c71321c508
Michel Vidailhet <michel.vidailhet@gmail.com>
2022-08-20 10:48:31 +0200

    Création de la méthode getCocktails

--- src/app/cocktail.service.ts
+++ src/app/cocktail.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Cocktail } from './cocktail.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CocktailService {
+  cocktails: Cocktail[] = [];
 
-  constructor() { }
+  getCocktails() {
+    return this.cocktails;
+  }
 }
