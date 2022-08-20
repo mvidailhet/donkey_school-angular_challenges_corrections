70fc05d3bcced4b1b603a2e23e160644e274a8cf
Michel Vidailhet <michel.vidailhet@gmail.com>
2022-08-20 13:03:56 +0200

    Création de la page playlist-detail permettant d'afficher les détails d'une playlist
    Ajout du lien pour y accéder depuis la page playlists
    
    Ajout d'un lien pour revenir à la page des playlists

--- src/app/pages/playlists/playlists.module.ts
+++ src/app/pages/playlists/playlists.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { PlaylistsRoutingModule } from './playlists/playlists-routing.module';
 import { PlaylistsComponent } from './playlists/playlists.component';
+import { PlaylistDetailComponent } from './playlist-detail/playlist-detail.component';
 
 @NgModule({
-  declarations: [PlaylistsComponent],
+  declarations: [PlaylistsComponent, PlaylistDetailComponent],
   imports: [CommonModule, PlaylistsRoutingModule, NgbModule],
   providers: [],
 })
--- src/app/pages/playlists/playlists/playlists-routing.module.ts
+++ src/app/pages/playlists/playlists/playlists-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PlaylistDetailComponent } from '../playlist-detail/playlist-detail.component';
 import { PlaylistsComponent } from './playlists.component';
 
 const routes: Routes = [
   {
     path: '',
     component: PlaylistsComponent
+  },
+  {
+    path: 'detail',
+    component: PlaylistDetailComponent
   }
 ];
 
